e4fdcdbbaeb4fb87e0c636d3aba1727397cdcf3b
Peter Calabrese <peter@calabrese.tech>
2021-06-21 18:56:02 -0400

    added delete function to list

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 class ToDo {
     constructor() {}
 
-    addToList(input) {
+    addToList(input, inputText) {
         let liDiv = document.createElement('div')
         let li = document.createElement('li')
         let del = document.createElement('BUTTON')
         liDiv.setAttribute('class', 'new-entry')
         del.setAttribute('div-entry', '')
         del.textContent = "Del"
-        li.textContent = input
+        li.textContent = inputText
         liDiv.append(li, del)
         toDoListUl.append(liDiv)
-        
+        input.innerText = ""
     }
 
-    delete(){
-        console.log("del")
+    delete(parentNode){
+        let userConfirmDelete = confirm(`Hold On! You are about to delete the following ToDo: \n ${parentNode.children[0].innerText}Are you sure you would like to continue?`)
+       
+        if(userConfirmDelete){
+             parentNode.remove()
+             
+        }
+        else return
+        
     }
 }
 
@@ -23,15 +30,23 @@ class ToDo {
 const submit = document.querySelector('[submit-button]')
 const newToDo = document.querySelector('input[new-todo]')
 const toDoListUl = document.querySelector('ul[item-list]')
-const deleteBtn = document.querySelectorAll('button[div-entry]')
+
 
 const toDo = new ToDo()
 
 
 submit.addEventListener('click', button => {
-    toDo.addToList(newToDo.value)
+    toDo.addToList(newToDo, newToDo.value)
+    const deleteBtn = document.querySelectorAll('button[div-entry]')
+   
+    deleteBtn.forEach(button =>
+        button.addEventListener('click', () => {
+           let deleteParentNode = button.parentNode
+           //let toDoText = 
+           toDo.delete(deleteParentNode)
+        }))
+    
+    
 })
 
-deleteBtn.addEventListener('click', button => {
-    toDo.delete()
-})
+
