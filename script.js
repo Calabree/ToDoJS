aac8eba5877d1eedd81565ce6311a6aa1aca7682
Peter Calabrese <peter@calabrese.tech>
2021-06-21 22:34:22 -0400

    Fixed bug with delete button giving multiple prompts

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 class ToDo {
     constructor() {}
 
-    addToList(input, inputText) {
+    addToList(input, inputText, button) {
         let liDiv = document.createElement('div')
         let li = document.createElement('li')
         let del = document.createElement('BUTTON')
         liDiv.setAttribute('class', 'new-entry')
-        del.setAttribute('div-entry', '')
+        //del.setAttribute('div-entry', '')
+        del.setAttribute('onclick', 'test(this)')
         del.textContent = "Del"
         li.textContent = inputText
         liDiv.append(li, del)
@@ -14,15 +15,14 @@ class ToDo {
         input.innerText = ""
     }
 
-    delete(parentNode){
-        let userConfirmDelete = confirm(`Hold On! You are about to delete the following ToDo: \n ${parentNode.children[0].innerText}Are you sure you would like to continue?`)
-       
-        if(userConfirmDelete){
-             parentNode.remove()
-             
-        }
-        else return
-        
+    delete(parentNode) {
+        var userConfirmDelete = confirm(`Hold On! You are about to delete the following ToDo: \n${parentNode.children[0].innerText}\nAre you sure you would like to continue?`)
+
+        if (userConfirmDelete) {
+            parentNode.remove()
+
+        } else return
+
     }
 }
 
@@ -30,23 +30,19 @@ class ToDo {
 const submit = document.querySelector('[submit-button]')
 const newToDo = document.querySelector('input[new-todo]')
 const toDoListUl = document.querySelector('ul[item-list]')
-
+let btn=null;
 
 const toDo = new ToDo()
 
 
-submit.addEventListener('click', button => {
-    toDo.addToList(newToDo, newToDo.value)
-    const deleteBtn = document.querySelectorAll('button[div-entry]')
-   
-    deleteBtn.forEach(button =>
-        button.addEventListener('click', () => {
-           let deleteParentNode = button.parentNode
-           //let toDoText = 
-           toDo.delete(deleteParentNode)
-        }))
-    
+submit.addEventListener('click', () => {
+    btn = toDo.addToList(newToDo, newToDo.value)
     
 })
 
+function test(elem) {
+    let deleteParentNode = elem.parentNode
+
+    toDo.delete(deleteParentNode)
+}
 
